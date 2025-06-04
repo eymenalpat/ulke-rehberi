0be55f1134a9fdb93fea96d01c2c178a0af79dc9
Eymen <eymenalpat0@gmail.com>
2025-06-05 00:07:35 +0300

    rest api'de field eklenmediği için hata alınıyordu bu çözülmek için field parametresi eklendi

--- utils/api.ts
+++ utils/api.ts
@@ -3,7 +3,9 @@ import axios from 'axios';
 const BASE_URL = 'https://restcountries.com/v3.1';
 
 export const fetchAllCountries = async () => {
-  const response = await axios.get(`${BASE_URL}/all`);
+  const response = await axios.get(
+    `${BASE_URL}/all?fields=name,flags,cca3,capital,population,region`
+  );
   return response.data;
 };
 
