b65ce31a64c0b057542070edd080e6344af68914
Eymen <eymenalpat0@gmail.com>
2025-06-05 00:03:52 +0300

    favoriler ekranı düzenlendi

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -1,9 +1,34 @@
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+import CountryCard from '../../components/CountryCard';
+import { useFavoritesStore } from '../../store/favoritesStore';
+import { useRouter } from 'expo-router';
 
 export default function FavoritesScreen() {
+  const { favorites, removeFavorite, isFavorite } = useFavoritesStore();
+  const router = useRouter();
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Favori Ülkeler Ekranı</Text>
+    <View style={{ flex: 1 }}>
+      {favorites.length === 0 ? (
+        <Text style={{ textAlign: 'center', marginTop: 40 }}>
+          Henüz favori ülke eklemediniz.
+        </Text>
+      ) : (
+        <FlatList
+          data={favorites}
+          keyExtractor={(item) => item.cca3}
+          renderItem={({ item }) => (
+            <CountryCard
+              name={item.name}
+              flag={item.flag}
+              isFavorite={isFavorite(item.cca3)}
+              onPress={() => router.push({ pathname: '/detail', params: { code: item.cca3 } })}
+              onToggleFavorite={() => removeFavorite(item.cca3)}
+            />
+          )}
+        />
+      )}
     </View>
   );
 } 
