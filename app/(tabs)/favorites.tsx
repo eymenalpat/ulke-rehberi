cbb3f89d7f312a97b9d67831976abc6cee0c0b5e
Eymen <eymenalpat0@gmail.com>
2025-06-05 00:01:13 +0300

    API bağlantıları eklendi, kullanıcı deneiymi için hata ve yükleniyor ekranları eklendi. Filtreleme eklendi.

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function FavoritesScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Favoriler Ekranı</Text>
+      <Text>Favori Ülkeler Ekranı</Text>
     </View>
   );
 } 
